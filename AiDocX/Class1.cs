8390719b4cfa752e19ba0b806f752629321460e9
SuslovAA <suslov_aa@outlook.com>
2024-04-22 04:27:05 +0300

    хардкод

---
+++ AiDocX/Class1.cs
@@ -0,0 +1,66 @@
+﻿//var addressElement = doc.Descendants("АдресРФ").FirstOrDefault() ??
+//                            doc.Descendants("СвАдрЮЛФИАС").FirstOrDefault();
+
+//if (addressElement != null)
+//{
+//    var index = addressElement.Attribute("Индекс")?.Value ?? "Индекс не найден";
+//    var regionCode = addressElement.Attribute("КодРегион")?.Value ?? "Код региона не найден";
+//    var house = addressElement.Attribute("Дом")?.Value ?? "Дом не найден";
+//    var building = addressElement.Attribute("Корпус")?.Value ?? "Корпус не найден";
+//    var regionName = addressElement.Element("Регион")?.Element("НаимРегион")?.Value ?? "Наименование региона не найдено";
+//    var streetType = addressElement.Element("Улица")?.Attribute("ТипУлица")?.Value ?? "";
+//    var streetName = addressElement.Element("Улица")?.Attribute("НаимУлица")?.Value ?? "Улица не найдена";
+
+//    Если код региона равен '77', то это Москва
+//    if (regionCode == "77")
+//    {
+//        regionName = "Г.МОСКВА";
+//    }
+
+//    Сборка строки адреса
+//   var addressParts = new List<string> { index, regionName, $"{streetType} {streetName}", house, building }
+//       .Where(part => !string.IsNullOrWhiteSpace(part))
+//       .ToArray();
+
+//    var fullAddress = string.Join(", ", addressParts);
+//    Console.WriteLine("Адрес: " + fullAddress);
+//}
+//else
+//{
+//    Console.WriteLine("Элемент адреса не найден в XML.");
+//}
+
+
+using System;
+using System.Net;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+
+    //    var elementsToFind = new List<string>
+    //    {
+    //        "Индекс", "КодРегион", "КодАдрКладр", "Дом", "Корпус", "Кварт", "Регион",
+    //        "Район", "Город", "НаселПункт", "Улица", "Района", "НаимРегион", "МуниципРайон",
+    //        "ГородСелПоселен", "НаселенПункт", "ЭлПланСтруктур", "ЭлУлДорСети", "Здание",
+    //        "ПомещЗдания", "ПомещКвартиры"
+    //    };
+
+    //    ParseAndPrintElements(doc.Root, elementsToFind);
+    //}
+
+    //static void ParseAndPrintElements(XElement element, List<string> elementsToFind)
+    //{
+    //    foreach (var elem in element.Elements())
+    //    {
+    //        if (elementsToFind.Contains(elem.Name.LocalName))
+    //        {
+    //            Console.WriteLine($"{elem.Name.LocalName}: {elem.Value}");
+    //            foreach (var attr in elem.Attributes())
+    //            {
+    //                Console.WriteLine($"{attr.Name.LocalName}: {attr.Value}");
+    //            }
+    //        }
+    //        ParseAndPrintElements(elem, elementsToFind);
+    //    }
+    //}
---
+++ AiDocX/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Net;
+using System.Xml.Linq;
+using System.Linq;
+
+//неккоректно улицу обрабатывает в <АдресРФ
+
+class Program
+{
+    static string url = "https://egrul.itsoft.ru/7730588444.xml";
+
+    static void Main()
+    {
+        string xmlContent = new WebClient().DownloadString(url);
+        XDocument doc = XDocument.Parse(xmlContent);
+
+        // Извлечение и вывод информации о компании
+        PrintCompanyInfo(doc);
+
+        // Извлечение и вывод адресной информации
+        PrintAddressInfo(doc);
+    }
+
+    static void PrintAddressInfo(XDocument doc)
+    {
+        var addressElements = doc.Descendants("СвАдресЮЛ")
+                                 .Union(doc.Descendants("СвАдрЮЛФИАС"))
+                                 .Union(doc.Descendants("АдресРФ"));
+
+        foreach (var addressElement in addressElements)
+        {
+            var descendantsAndSelf = addressElement.DescendantsAndSelf().ToList();
+            var data = new Dictionary<string, string>();
+
+            // Добавление информации в словарь, если она не равна null
+            data.TryAdd("Индекс", descendantsAndSelf.Attributes("Индекс").FirstOrDefault()?.Value);
+            data.TryAdd("Код региона", descendantsAndSelf.Attributes("КодРегион").FirstOrDefault()?.Value);
+            data.TryAdd("Код адреса по КЛАДР", descendantsAndSelf.Attributes("КодАдрКладр").FirstOrDefault()?.Value);
+            data.TryAdd("Дом", descendantsAndSelf.Attributes("Дом").FirstOrDefault()?.Value);
+            data.TryAdd("Корпус", descendantsAndSelf.Attributes("Корпус").FirstOrDefault()?.Value);
+            data.TryAdd("Квартира", descendantsAndSelf.Attributes("Кварт").FirstOrDefault()?.Value);
+            var regionName = descendantsAndSelf.Elements("НаимРегион").FirstOrDefault()?.Value;
+            if (string.IsNullOrEmpty(regionName))
+            {
+                regionName = descendantsAndSelf.Elements("Регион").Select(e => e.Attribute("НаимРегион")?.Value ?? e.Value).FirstOrDefault();
+            }
+            data.TryAdd("Регион", regionName);
+            data.TryAdd("Район", descendantsAndSelf.Elements("МуниципРайон").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
+            data.TryAdd("Город", descendantsAndSelf.Elements("Город").Select(e => e.Attribute("НаимГород")?.Value).FirstOrDefault());
+            data.TryAdd("Населенный пункт", descendantsAndSelf.Elements("НаселПункт").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
+            data.TryAdd("Улица", descendantsAndSelf.Elements("ЭлУлДорСети").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
+            data.TryAdd("Дата ГРН", descendantsAndSelf.Attributes("ГРНДата").FirstOrDefault()?.Value);
+            data.TryAdd("Дата исправления ГРН", descendantsAndSelf.Attributes("ГРНДатаИспр").FirstOrDefault()?.Value);
+            data.TryAdd("Идентификационный номер", descendantsAndSelf.Attributes("ИдНом").FirstOrDefault()?.Value);
+            data.TryAdd("Городское/сельское поселение", descendantsAndSelf.Attributes("ГородСелПоселен").FirstOrDefault()?.Value);
+            data.TryAdd("Населенный пункт", descendantsAndSelf.Attributes("НаселенПункт").FirstOrDefault()?.Value);
+            data.TryAdd("Элемент планировочной структуры", descendantsAndSelf.Attributes("ЭлПланСтруктур").FirstOrDefault()?.Value);
+            data.TryAdd("Элемент улично-дорожной сети", descendantsAndSelf.Attributes("ЭлУлДорСети").FirstOrDefault()?.Value);
+            var buildings = descendantsAndSelf.Elements("Здание").Select(e => $"Тип: {e.Attribute("Тип")?.Value}, Номер: {e.Attribute("Номер")?.Value}").ToList();
+            data.TryAdd("Помещение здания", descendantsAndSelf.Elements("ПомещЗдания").Select(e => e.Attribute("Номер")?.Value).FirstOrDefault());
+            data.TryAdd("Помещение квартиры", descendantsAndSelf.Elements("ПомещКвартиры").Select(e => e.Attribute("Номер")?.Value).FirstOrDefault());
+
+            // Вывод словаря в консоль
+            foreach (var pair in data)
+            {
+                if (!string.IsNullOrEmpty(pair.Value))
+                {
+                    Console.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+            }
+
+            // Вывод всех зданий
+            foreach (var building in buildings)
+            {
+                Console.WriteLine($"Здание: {building}");
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+    static void PrintCompanyInfo(XDocument doc)
+    {
+        var shortNameElement = doc.Descendants("СвНаимЮЛСокр").FirstOrDefault();
+        string shortName = shortNameElement != null ? shortNameElement.Attribute("НаимСокр")?.Value : "Краткое название не найдено";
+
+        var fullNameElement = doc.Descendants("СвНаимЮЛ").FirstOrDefault();
+        string fullName = fullNameElement != null ? fullNameElement.Attribute("НаимЮЛПолн")?.Value : "Полное наименование не найдено";
+
+        var svYulElement = doc.Descendants("СвЮЛ").FirstOrDefault();
+        string inn = svYulElement?.Attribute("ИНН")?.Value ?? "ИНН не найден";
+        string ogrn = svYulElement?.Attribute("ОГРН")?.Value ?? "ОГРН не найден";
+        string kpp = svYulElement?.Attribute("КПП")?.Value ?? "КПП не найден";
+
+        var directorInfoElement = doc.Descendants("СвФЛ").FirstOrDefault();
+        string directorFullName = directorInfoElement != null
+            ? $"{directorInfoElement.Attribute("Фамилия")?.Value} {directorInfoElement.Attribute("Имя")?.Value} {directorInfoElement.Attribute("Отчество")?.Value}"
+            : "ФИО руководителя не найдено";
+
+        var directorPositionElement = doc.Descendants("СвДолжн").FirstOrDefault();
+        string directorPosition = directorPositionElement != null
+            ? directorPositionElement.Attribute("НаимДолжн")?.Value
+            : "Должность руководителя не найдена";
+
+        Console.WriteLine("ФИО руководителя: " + directorFullName);
+        Console.WriteLine("Должность руководителя: " + directorPosition);
+        Console.WriteLine("ИНН: " + inn);
+        Console.WriteLine("ОГРН: " + ogrn);
+        Console.WriteLine("КПП: " + kpp);
+        Console.WriteLine("Краткое название: " + shortName);
+        Console.WriteLine("Полное наименование: " + fullName);
+    }
+}
