8a8516886657bdc4cb1dbfa6caa87b2cc75e499a
SuslovAA <suslov_aa@outlook.com>
2024-04-23 07:50:58 +0300

    обход foreach всего,  а не ручной поиск элементов

--- AiDocX/Program.cs
+++ AiDocX/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Xml.Linq;
-using System.Linq;
-
-//неккоректно улицу обрабатывает в <АдресРФ
 
 class Program
 {
-    static string url = "https://egrul.itsoft.ru/7730588444.xml";
+    static string url = "https://egrul.itsoft.ru/7736050003.xml";
 
     static void Main()
     {
@@ -15,7 +14,7 @@ class Program
         XDocument doc = XDocument.Parse(xmlContent);
 
         // Извлечение и вывод информации о компании
-        PrintCompanyInfo(doc);
+        // PrintCompanyInfo(doc);
 
         // Извлечение и вывод адресной информации
         PrintAddressInfo(doc);
@@ -24,95 +23,37 @@ class Program
     static void PrintAddressInfo(XDocument doc)
     {
         var addressElements = doc.Descendants("СвАдресЮЛ")
-                                 .Union(doc.Descendants("СвАдрЮЛФИАС"))
-                                 .Union(doc.Descendants("АдресРФ"));
+                              .Union(doc.Descendants("СвАдрЮЛФИАС"))
+                              .Union(doc.Descendants("АдресРФ"));
 
-        foreach (var addressElement in addressElements)
+        foreach (var element in addressElements)
         {
-            var descendantsAndSelf = addressElement.DescendantsAndSelf().ToList();
-            var data = new Dictionary<string, string>();
-
-            // Добавление информации в словарь, если она не равна null
-            data.TryAdd("Индекс", descendantsAndSelf.Attributes("Индекс").FirstOrDefault()?.Value);
-            data.TryAdd("Код региона", descendantsAndSelf.Attributes("КодРегион").FirstOrDefault()?.Value);
-            data.TryAdd("Код адреса по КЛАДР", descendantsAndSelf.Attributes("КодАдрКладр").FirstOrDefault()?.Value);
-            data.TryAdd("Дом", descendantsAndSelf.Attributes("Дом").FirstOrDefault()?.Value);
-            data.TryAdd("Корпус", descendantsAndSelf.Attributes("Корпус").FirstOrDefault()?.Value);
-            data.TryAdd("Квартира", descendantsAndSelf.Attributes("Кварт").FirstOrDefault()?.Value);
-            var regionName = descendantsAndSelf.Elements("НаимРегион").FirstOrDefault()?.Value;
-            if (string.IsNullOrEmpty(regionName))
-            {
-                regionName = descendantsAndSelf.Elements("Регион").Select(e => e.Attribute("НаимРегион")?.Value ?? e.Value).FirstOrDefault();
-            }
-            data.TryAdd("Регион", regionName);
-            data.TryAdd("Район", descendantsAndSelf.Elements("МуниципРайон").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
-            data.TryAdd("Город", descendantsAndSelf.Elements("Город").Select(e => e.Attribute("НаимГород")?.Value).FirstOrDefault());
-            data.TryAdd("Населенный пункт", descendantsAndSelf.Elements("НаселПункт").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
-            data.TryAdd("Улица", descendantsAndSelf.Elements("ЭлУлДорСети").Select(e => e.Attribute("Наим")?.Value).FirstOrDefault());
-            data.TryAdd("Дата ГРН", descendantsAndSelf.Attributes("ГРНДата").FirstOrDefault()?.Value);
-            data.TryAdd("Дата исправления ГРН", descendantsAndSelf.Attributes("ГРНДатаИспр").FirstOrDefault()?.Value);
-            data.TryAdd("Идентификационный номер", descendantsAndSelf.Attributes("ИдНом").FirstOrDefault()?.Value);
-            data.TryAdd("Городское/сельское поселение", descendantsAndSelf.Attributes("ГородСелПоселен").FirstOrDefault()?.Value);
-            data.TryAdd("Населенный пункт", descendantsAndSelf.Attributes("НаселенПункт").FirstOrDefault()?.Value);
-            data.TryAdd("Элемент планировочной структуры", descendantsAndSelf.Attributes("ЭлПланСтруктур").FirstOrDefault()?.Value);
-            data.TryAdd("Элемент улично-дорожной сети", descendantsAndSelf.Attributes("ЭлУлДорСети").FirstOrDefault()?.Value);
-            var buildings = descendantsAndSelf.Elements("Здание").Select(e => $"Тип: {e.Attribute("Тип")?.Value}, Номер: {e.Attribute("Номер")?.Value}").ToList();
-            data.TryAdd("Помещение здания", descendantsAndSelf.Elements("ПомещЗдания").Select(e => e.Attribute("Номер")?.Value).FirstOrDefault());
-            data.TryAdd("Помещение квартиры", descendantsAndSelf.Elements("ПомещКвартиры").Select(e => e.Attribute("Номер")?.Value).FirstOrDefault());
-
-            // Вывод словаря в консоль
-            foreach (var pair in data)
-            {
-                if (!string.IsNullOrEmpty(pair.Value))
-                {
-                    Console.WriteLine($"{pair.Key}: {pair.Value}");
-                }
-            }
-
-            // Вывод всех зданий
-            foreach (var building in buildings)
-            {
-                Console.WriteLine($"Здание: {building}");
-            }
+            PrintElementAndChildren(element, 0);
         }
-
-
-
-
-
-
-
     }
 
-    static void PrintCompanyInfo(XDocument doc)
+    static void PrintElementAndChildren(XElement element, int indentLevel)
     {
-        var shortNameElement = doc.Descendants("СвНаимЮЛСокр").FirstOrDefault();
-        string shortName = shortNameElement != null ? shortNameElement.Attribute("НаимСокр")?.Value : "Краткое название не найдено";
-
-        var fullNameElement = doc.Descendants("СвНаимЮЛ").FirstOrDefault();
-        string fullName = fullNameElement != null ? fullNameElement.Attribute("НаимЮЛПолн")?.Value : "Полное наименование не найдено";
-
-        var svYulElement = doc.Descendants("СвЮЛ").FirstOrDefault();
-        string inn = svYulElement?.Attribute("ИНН")?.Value ?? "ИНН не найден";
-        string ogrn = svYulElement?.Attribute("ОГРН")?.Value ?? "ОГРН не найден";
-        string kpp = svYulElement?.Attribute("КПП")?.Value ?? "КПП не найден";
-
-        var directorInfoElement = doc.Descendants("СвФЛ").FirstOrDefault();
-        string directorFullName = directorInfoElement != null
-            ? $"{directorInfoElement.Attribute("Фамилия")?.Value} {directorInfoElement.Attribute("Имя")?.Value} {directorInfoElement.Attribute("Отчество")?.Value}"
-            : "ФИО руководителя не найдено";
-
-        var directorPositionElement = doc.Descendants("СвДолжн").FirstOrDefault();
-        string directorPosition = directorPositionElement != null
-            ? directorPositionElement.Attribute("НаимДолжн")?.Value
-            : "Должность руководителя не найдена";
-
-        Console.WriteLine("ФИО руководителя: " + directorFullName);
-        Console.WriteLine("Должность руководителя: " + directorPosition);
-        Console.WriteLine("ИНН: " + inn);
-        Console.WriteLine("ОГРН: " + ogrn);
-        Console.WriteLine("КПП: " + kpp);
-        Console.WriteLine("Краткое название: " + shortName);
-        Console.WriteLine("Полное наименование: " + fullName);
+        PrintElement(element, indentLevel);
+        foreach (var childElement in element.Elements())
+        {
+            PrintElementAndChildren(childElement, indentLevel + 1);
+        }
     }
+
+    static void PrintElement(XElement element, int indentLevel)
+    {
+        var indent = new string(' ', indentLevel * 2);
+        Console.WriteLine($"{indent}{element.Name.LocalName}:");
+        foreach (var attr in element.Attributes())
+        {
+            Console.WriteLine($"{indent}  {attr.Name.LocalName}: {attr.Value}");
+        }
+        if (!string.IsNullOrEmpty(element.Value))
+        {
+            Console.WriteLine($"{indent}  Value: {element.Value}");
+        }
+    }
+
+    // ... оставшаяся часть вашего кода ...
 }
--- AiDocX/obj/Debug/net8.0/AiDocX.AssemblyInfo.cs
+++ AiDocX/obj/Debug/net8.0/AiDocX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiDocX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8390719b4cfa752e19ba0b806f752629321460e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiDocX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiDocX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
